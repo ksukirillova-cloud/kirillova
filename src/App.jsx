0be52554232d4a17f760736290b8676f93289b5e
Kseniya <ksu.kirillova@gmail.com>
2026-05-30 14:41:06 +0300

    add ksenia photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import imgKsenia from './assets/ksenia-photo.jpg';
+import imgKsenia from './assets/ksenia-photo.png';
 import {
   ArrowLeft,
   ArrowRight,
