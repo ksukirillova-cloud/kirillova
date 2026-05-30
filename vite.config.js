4678eb9c3dc23c5cbafe1965dcdcc89431d35814
Kseniya <ksu.kirillova@gmail.com>
2026-05-30 11:10:34 +0300

    react-snap prerender

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import prerender from 'vite-plugin-prerender'
-import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: ['/'],
-    }),
-  ],
+  plugins: [react()],
 })
