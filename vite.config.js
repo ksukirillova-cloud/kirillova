77e308d6716ff3d14c6040ba7fc67f715a8a0f81
Kseniya <ksu.kirillova@gmail.com>
2026-05-30 10:55:53 +0300

    add prerender for SEO

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import prerender from 'vite-plugin-prerender'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    prerender({
+      staticDir: path.join(__dirname, 'dist'),
+      routes: ['/'],
+    }),
+  ],
+})
